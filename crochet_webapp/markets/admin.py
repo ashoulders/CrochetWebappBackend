90eb62296af18fe900b73d4bdca52803c93f9d9d
ashoulders <ashoulders@outlook.com>
2024-02-20 20:57:34 +0000

    Setup of models + admin site

--- crochet_webapp/markets/admin.py
+++ crochet_webapp/markets/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from markets.models import *
 
-# Register your models here.
+class MarketAdmin(admin.ModelAdmin):
+    list_display = ['name', 'date', 'location']
+
+class StockItemAdmin(admin.ModelAdmin):
+    list_display = ['pattern_name', 'standard_price']
+
+class StockVariantAdmin(admin.ModelAdmin):
+    list_display = ['get_item_name', 'variant', 'current_stock']
+
+    def get_item_name(self, obj):
+        return obj.item.name
+    
+class MarketStockAdmin(admin.ModelAdmin):
+    list_display = ['get_market_name', 'get_item_name', 'number']
+
+    def get_market_name(self, obj):
+        return obj.market.date
+
+    def get_item_name(self, obj):
+        return obj.item.name
+    
+class ItemSoldAdmin(admin.ModelAdmin):
+    list_display = ['get_item_name', 'price', 'number']
+
+    def get_item_name(self, obj):
+        return obj.market_item.item.name
+
+admin.site.register(Market, MarketAdmin)
+admin.site.register(StockItem, StockItemAdmin)
+admin.site.register(StockVariant, StockVariantAdmin)
+admin.site.register(MarketStock, MarketStockAdmin)
+admin.site.register(ItemSold, ItemSoldAdmin)
--- crochet_webapp/markets/models.py
+++ crochet_webapp/markets/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class Market(models.Model):
+    name = models.CharField(max_length=128)
+    date = models.DateField()
+    location = models.CharField(max_length=128)
+
+class StockItem(models.Model):
+    pattern_name = models.CharField(max_length=128)
+    standard_price = models.DecimalField(max_digits=5, decimal_places=2)
+    picture = models.ImageField(blank=True, null=True)
+
+# e.g. colour 
+class StockVariant(models.Model):
+    item = models.ForeignKey(StockItem, on_delete=models.CASCADE)
+    variant = models.CharField(max_length=256)
+    current_stock = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+
+class MarketStock(models.Model):
+    market = models.ForeignKey(Market, on_delete=models.CASCADE)
+    item = models.ForeignKey(StockVariant, on_delete=models.CASCADE)
+    number = models.IntegerField(validators=[MinValueValidator(0)])
+
+# this table exists as price may differ per item sold, even if it is the same item
+# for example in a deal e.g 2 for £10
+class ItemSold(models.Model):
+    market_item = models.ForeignKey(StockVariant, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    number = models.IntegerField(validators=[MinValueValidator(0)])
